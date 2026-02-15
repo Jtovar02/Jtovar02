03f161ba62d465e5be65231406d3c5ba7e3c3892
Jy02 <jtovar02@gmail.com>
2026-02-15 16:38:07 +0100

    Primer commit - estructura inicial

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+a = 'My name is Jesus'
+b = "This i also a string"
+
+print(a,b)
---
+++ jsonscript.py
@@ -0,0 +1,9 @@
+sample = {
+  "service": "journal-api",
+  "status": "healthy",
+  "region": "eastus",
+  "requests_last_hour": 128,
+}
+
+for key, value in sample.items():
+    print(f"{key}: {value}")
